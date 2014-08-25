6ba1518fba89fa4b03c9b323c74b47aaa34e7cce
zaighumrajput <zaighum47@gmail.com>
2014-08-25 21:25:53 +0200

    added

--- tango/tango_with_django_project/tango_with_django_project/populate_rango.py
+++ tango/tango_with_django_project/tango_with_django_project/populate_rango.py
@@ -15,6 +15,21 @@ def populate():
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
     
+    add_page(cat=django_cat,
+        title="Official Django Tutorial",
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+
+    add_page(cat=django_cat,
+        title="Django Rocks",
+        url="http://www.djangorocks.com/")
+
+    add_page(cat=django_cat,
+        title="How to Tango with Django",
+        url="http://www.tangowithdjango.com/")
+
+    frame_cat = add_cat("Other Frameworks")
+    
+    
     
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0] #wtf is this 0 for, ugly
